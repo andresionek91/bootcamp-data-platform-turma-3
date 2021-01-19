a26d5e1e30757ad0052da868d219e55bda555d51
Andre Sionek <andresionek91@gmail.com>
2021-01-19 23:29:32 +0000

    DE-00/Cria stack do athena

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from athena.stack import AthenaStack
 from aws_cdk import core
 
 from bootcamp_data_platform_turma_3.data_lake.stack import DataLakeStack
@@ -6,4 +7,5 @@ from glue_catalog.stack import GlueCatalogStack
 app = core.App()
 data_lake = DataLakeStack(app)
 glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
+athena_stack = AthenaStack(app)
 app.synth()
