689f5b102854dea4029086afac44d05d77ca39c3
Andre Sionek <andresionek91@gmail.com>
2021-01-19 23:36:58 +0000

    DE-00/Cria stack kinesis

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from aws_cdk import core
 
 from bootcamp_data_platform_turma_3.data_lake.stack import DataLakeStack
 from glue_catalog.stack import GlueCatalogStack
+from kinesis.stack import KinesisStack
 
 app = core.App()
+
 data_lake = DataLakeStack(app)
 glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
 athena_stack = AthenaStack(app)
+kinesis_stack = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
+
 app.synth()
