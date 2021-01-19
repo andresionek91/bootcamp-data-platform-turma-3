69ab65c7ad82c863290037fe0fc9d912c0a864dd
Andre Sionek <andresionek91@gmail.com>
2021-01-19 23:23:02 +0000

    DE-00/Arruma stack

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from aws_cdk import core
 
 from bootcamp_data_platform_turma_3.data_lake.stack import DataLakeStack
+from glue_catalog.stack import GlueCatalogStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
+glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
 app.synth()
