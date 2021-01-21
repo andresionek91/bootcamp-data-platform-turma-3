66b94aeae21e74039959f6e3d88c8218a01d3ba2
Andre Sionek <andresionek91@gmail.com>
2021-01-21 21:40:04 +0000

    DE-00/Adiciona stack no app

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from bootcamp_data_platform_turma_3.data_lake.stack import DataLakeStack
 from glue_catalog.stack import GlueCatalogStack
 from kinesis.stack import KinesisStack
 
+from common_stack import CommonStack
+
 app = core.App()
 
 data_lake = DataLakeStack(app)
 glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
 athena_stack = AthenaStack(app)
 kinesis_stack = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
-
+common_stack = CommonStack(app)
 app.synth()
